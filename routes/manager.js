2c24d5e12583f24d7aeb5939fb8aab1fa7d1a41f
pmy09 <69659683+pmy09@users.noreply.github.com>
2021-01-03 16:57:39 +0000

    Update manager.js

--- routes/manager.js
+++ routes/manager.js
@@ -12,10 +12,22 @@ router.get('/manager', isAuth, managerController.getManager);
 
 router.get('/form', isAuth, managerController.getForm);
 
+router.get('/form', isAuth, managerController.getEditUser);
+
 router.get('/crew', isAuth, managerController.getCrew);
 
 router.get('/chart', isAuth, managerController.getChart);
 
-router.get('/users', isAuth, managerController.getUsers);
+// router.get('/users', isAuth, managerController.getUsers);
+
+router.get('/deluser/:userId', managerController.delUser);
+
+// router.get('/approve', managerController.approveUser);
+
+router.get('/edit-user/:userId', isAuth, managerController.getEditUser);
+
+// router.post('/edit-user', isAuth, managerController.postEditUser);
+
+// router.post('/approve', managerController.postEditUser)
 
 module.exports = router;
