ae0e05938cd71760437a3a72b7c0ef402252d06c
pmy09 <69659683+pmy09@users.noreply.github.com>
2021-01-03 16:57:22 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,12 +3,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  username : {
+    type : String,
+    required : true,
+  },
   email: {
     type: String,
     required: true
   },
   password: {
     type: String,
+    required: true
+  },
+  role : {
+    type : String,
+    required : true,
+  },
+  disabled: {
+    type: Boolean,
     required: true
   },
   cart: {
