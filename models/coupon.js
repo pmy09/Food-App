f481b9c309be7203e9a1afea4358bd4005430b09
pmy09 <69659683+pmy09@users.noreply.github.com>
2021-01-06 11:16:42 +0000

    Update coupon.js

--- models/coupon.js
+++ models/coupon.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const couponSchema = new Schema({});
+const couponSchema = new Schema({
+    code : {
+        type : String,
+        required : true
+    },
+    date : {
+        type : String,
+        required : true
+    }
+});
 
 module.exports = mongoose.model('Coupon', couponSchema);
